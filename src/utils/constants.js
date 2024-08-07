b82d2e044b05315506b92f7e07cdf04074c5d02a
RishRaj0422 <92047392+RishRaj0422@users.noreply.github.com>
2024-08-07 21:57:41 +0530

    fav icon update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const CDN_LOGO =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
 
 export const MENU_API =
-"https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
